486c2ad84b25c9f5d1e899e783f6e7b3b1740074
김재원 <wodnjs9058@naver.com>
2025-06-13 15:23:43 +0900

    init

---
+++ src/main/java/com/example/bulletin_board/BulletinBoardApplication.java
@@ -0,0 +1,14 @@
+package com.example.bulletin_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulletinBoardApplication {
+
+	public static void main(String[] args) {
+		System.out.println("실행 확인");
+		//SpringApplication.run(BulletinBoardApplication.class, args);
+	}
+
+}

597e26cb8bc25cd8715c97519a357a5289b02085
김재원 <wodnjs9058@naver.com>
2025-06-14 12:36:11 +0900

    Database connected

--- src/main/java/com/example/bulletin_board/BulletinBoardApplication.java
+++ src/main/java/com/example/bulletin_board/BulletinBoardApplication.java
@@ -3,12 +3,17 @@ package com.example.bulletin_board;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+사용자 board_manager
+비밀번호 root
+DB이름 boarddb
+ */
 @SpringBootApplication
 public class BulletinBoardApplication {
 
 	public static void main(String[] args) {
-		System.out.println("실행 확인");
-		//SpringApplication.run(BulletinBoardApplication.class, args);
+		//System.out.println("실행 확인");
+		SpringApplication.run(BulletinBoardApplication.class, args);
 	}
 
 }

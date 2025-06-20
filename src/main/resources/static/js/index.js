fcca24824baa3c4428b4ed6d9cb4e9f0348081d0
김재원 <wodnjs9058@naver.com>
2025-06-20 17:40:57 +0900

    Completed frontend main page

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,8 @@
+let link = "http://localhost:8000/main.html";
+
+document.addEventListener("DOMContentLoaded",moveToMain);
+function moveToMain(){
+    console.log("connected");
+    fetch("http://localhost:8000/")
+    .then(location.replace(link));
+}
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,7 +1,36 @@
-document.addEventListener("DOMContentLoaded",moveToMain);
+addEventListener("DOMContentLoaded",loadPosts);
 
-function moveToMain(){
-    fetch("localhost:8000")
-    .then(console.log("패치 데이터"))
-    .error((e) => console.error("error occurred: e"));
+function loadPosts(){
+    console.log("'posts' get request");
+    fetch("http://localhost:8000/main")
+    .then(response => response.json())
+    .then(posts => {
+        const postTag = document.getElementById("post");
+        const ol = document.createElement("ol");
+
+        posts.forEach((post) => {
+            const li = document.createElement("li");
+            const title = document.createElement("a");
+            const editBtn = document.createElement("button");
+            let individualPost = post.postId;
+
+            title.textContent = post.title;
+            title.href="http://localhost:8000/main/"+ individualPost;
+            editBtn.textContent = "수정";
+            editBtn.type="button";
+
+            editBtn.addEventListener("click",() =>{
+                window.location.href="http://localhost:8000/edit.html";});
+
+            li.classList.add("post-item");
+            title.classList.add("post-title");
+            editBtn.classList.add("edit-button");
+
+            li.appendChild(title);
+            li.appendChild(editBtn);
+            ol.appendChild(li);
+        });
+        postTag.appendChild(ol);
+    })
+    .catch((e) => console.error("'posts' get request fetch error:",e));
 }

1c483bc63998e44bad5dcca1867073c1337f29f8
김재원 <wodnjs9058@naver.com>
2025-10-31 23:14:57 +0900

    auth:security configuration and default user setting

--- src/main/java/com/example/bulletin_board/common/SecurityConfig.java
+++ src/main/java/com/example/bulletin_board/common/SecurityConfig.java
@@ -1,13 +1,48 @@
 package com.example.bulletin_board.common;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    //25-10-31
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/","/api/join","/api/login").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/posts").permitAll()
+                        .requestMatchers("/api/**").hasRole("USER")
+                        .anyRequest().authenticated()
+                );
+        //25-10-31
+        //공부 필요-AI가 작성한 코드
+        httpSecurity
+                .logout((logout) -> logout
+                        .logoutUrl("/api/logout")
+                        .logoutSuccessHandler((request, response, authentication) -> {
+                            response.setStatus(HttpServletResponse.SC_OK);
+                        })
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                );
 
+        //csrf꺼놓음
+        httpSecurity
+                .csrf((auth) -> auth.disable());
+
+        return httpSecurity.build();
+    }
+
+//    //25-10-31
+//    @Bean
+//    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
 }
